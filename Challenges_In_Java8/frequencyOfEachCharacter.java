e0518145da073f24ed08ce59f95195d8cc65d199
Sundarleo11 <sundarrock1999@gmail.com>
2024-02-19 21:27:46 +0530

    updated some logic

--- Challenges_In_Java8/frequencyOfEachCharacter.java
+++ Challenges_In_Java8/frequencyOfEachCharacter.java
@@ -1,22 +1,39 @@
 package Challenges_In_Java8;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class frequencyOfEachCharacter {
-	
-	//How do you find frequency of each character in a string using Java 8 streams?
+
+	// How do you find frequency of each character in a string using Java 8 streams?
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		String inputString = "Java Concept Of The Day";
 
+		// first came for space 4 as count
 		Map<Character, Long> counts = inputString.chars().mapToObj(c -> (char) c)
 				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-		System.out.println(counts);
+		/*
+		 * The main difference between HashMap and LinkedHashMap is that LinkedHashMap
+		 * maintains
+		 * the insertion order of keys, the order in which keys are inserted into
+		 * LinkedHashMap.
+		 * On the other hand, HashMap doesn't maintain any order or keys, or values.
+		 */
+		Map<String, Long> count1 = Arrays.stream(inputString.split("")).map(String::toLowerCase)
+				.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
+
+		System.out.println("count1" + count1);
+		System.out.println("counts" + counts);
+
+		// count1{j=1, a=3, v=1, =4, c=2, o=2, n=1, e=2, p=1, t=2, f=1, h=1, d=1, y=1}
+		// counts{ =4, a=3, c=1, C=1, D=1, e=2, f=1, h=1, J=1, n=1, O=1, o=1, p=1, T=1,
+		// t=1, v=1, y=1}
 	}
 
 }
--- Challenges_In_Java8/frequencyOfEachElement.java
+++ Challenges_In_Java8/frequencyOfEachElement.java
@@ -1,6 +1,7 @@
 package Challenges_In_Java8;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -12,10 +13,19 @@ public class frequencyOfEachElement {
 		// TODO Auto-generated method stub
 		List<String> stationeryList = Arrays.asList("Pen", "Eraser", "Note Book", "Pen", "Pencil", "Stapler",
 				"Note Book", "Pencil");
-		//Map the List of each words with count
+		// Map the List of each words with count
 		Map<String, Long> values = stationeryList.stream()
 				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-		System.out.println(values);
+
+		// Duplicate words
+		Map<String, Long> words = stationeryList.stream().filter(s -> Collections.frequency(stationeryList, s) > 1)
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+		System.out.println("world" + words);
+		System.out.println("values" + values);
+
+		// world{Pen=2, Pencil=2, Note Book=2}
+		// values{Pen=2, Stapler=1, Pencil=2, Note Book=2, Eraser=1}
 
 	}
 
